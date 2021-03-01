a44d3cbbce53b52439228e47c97f60723f464664
Den 375 <denisoff375@gmail.com>
2021-03-01 16:48:09 +0300

    75. installed yarn, added redux-form, created LoginForm with reduxForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import {Field, reduxForm} from "redux-form";
 
-const Login = (props) => {
+const LoginForm = props => {
+    return <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field name="login" placeholder='Введите login' component="input" type="text" />
+            </div>
+            <div>
+                <Field name="password" placeholder='Введите пароль' component="input" type="text" />
+            </div>
+            <div>
+                <Field name="rememberMe" component="input" type="checkbox" /> Запомнить меня
+            </div>
+            <div>
+                <button>Войти</button>
+            </div>
+           </form>
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+const Login = props => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
     return <div>
-        <form action="">
-            <label htmlFor="">Введите логин</label>
-            <input type="text"/>
-        </form>
-        <form action="">
-            <label htmlFor="">Введите пароль</label>
-            <input type="text"/>
-        </form>
-
-          </div>
+             <h1>Login</h1>
+             <LoginReduxForm onSubmit={onSubmit}/>
+           </div>
 }
 
 export default Login;
+
