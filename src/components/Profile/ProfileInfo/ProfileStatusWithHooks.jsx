bada7c675d1092b5e31a17f8192e75754ee5f6f3
Den 375 <denisoff375@gmail.com>
2021-03-10 20:34:20 +0300

    85 - React JS - hook, useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+// когда придет новый статус хуки уже были вызваны и их результат храниться в реакт, при смене пропсов и новом
+//вызове ф-ии они повторно не будут вызываться. В span статус напрямую из пропсов измениться, а в input нет. Нужно синхронизировать
+//используем для этого useEffect
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
