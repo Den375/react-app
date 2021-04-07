5f3c231e3b98c0fbaefe36b4dcee831d5dd21368
Den375 <denisoff375@gmail.com>
2021-04-07 19:44:26 +0300

    92 - тестируем компоненты, тесты, react-test-renderer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Route, withRouter} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
@@ -9,34 +9,35 @@ import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import Preloader from "./components/common/preloader/Preloader";
 import {compose} from "redux";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
+import store from "./redux/redux-store";
 
-class App extends React.Component  {
+class App extends React.Component {
 
     componentDidMount() {
         this.props.initializeApp()
     }
 
-   render() {
-        if(!this.props.initialized) {
+    render() {
+        if (!this.props.initialized) {
             return <Preloader/>
         }
 
-       return (
-           <div className='app-wrapper'>
-               <HeaderContainer/>
-               <Navbar/>
-               <div className='app-wrapper-content'>
-                   <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                   <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                   <Route path='/users' render={() => <UsersContainer/>}/>
-                   <Route path='/login' render={() => <LoginPage/>}/>
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/login' render={() => <LoginPage/>}/>
 
-               </div>
-           </div>
-       )
-   }
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
@@ -45,9 +46,23 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default compose(
+const AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App);
 
+const SocialNetwork = props => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>
+
+
+}
+
+export default SocialNetwork;
+
+
 /*'/profile/:userId?'  profile/ тоже рендерит благодаря ?
 параметр становится опциональным*/
+
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import store from "./redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import SocialNetwork from './App';
 
-ReactDOM.render(<BrowserRouter>
-                      <Provider store={store}>
-                           <App/>
-                        </Provider>
-                    </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<SocialNetwork/>, document.getElementById('root'));
 
 /*store.subscribe(rerenderEntireTree);*/
 
