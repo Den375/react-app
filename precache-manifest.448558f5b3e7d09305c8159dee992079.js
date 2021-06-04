7594210769c1555260b6ed6b5084d2a81d422de6
Den375 <denisoff375@gmail.com>
2021-06-04 13:11:23 +0300

    Updates

---
+++ precache-manifest.448558f5b3e7d09305c8159dee992079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "821462f1973bfe220d5a",
+    "url": "/react-app/static/css/main.1f3f0b10.chunk.css"
+  },
+  {
+    "revision": "821462f1973bfe220d5a",
+    "url": "/react-app/static/js/main.821462f1.chunk.js"
+  },
+  {
+    "revision": "b42e33b01e27c297ce69",
+    "url": "/react-app/static/css/1.cc27f500.chunk.css"
+  },
+  {
+    "revision": "b42e33b01e27c297ce69",
+    "url": "/react-app/static/js/1.b42e33b0.chunk.js"
+  },
+  {
+    "revision": "0a1ff0ccabd604cafda7",
+    "url": "/react-app/static/css/2.7de38f9e.chunk.css"
+  },
+  {
+    "revision": "0a1ff0ccabd604cafda7",
+    "url": "/react-app/static/js/2.0a1ff0cc.chunk.js"
+  },
+  {
+    "revision": "3f9d0fd7f3f9b5926f07",
+    "url": "/react-app/static/js/3.3f9d0fd7.chunk.js"
+  },
+  {
+    "revision": "7a1bab0c1e6fe286ae34",
+    "url": "/react-app/static/js/runtime~main.7a1bab0c.js"
+  },
+  {
+    "revision": "1088ab62b3271e6f12b72ee60c56e3f7",
+    "url": "/react-app/static/media/126.1088ab62.svg"
+  },
+  {
+    "revision": "f31d8efd1106751a7b45c2b43fee636c",
+    "url": "/react-app/index.html"
+  }
+];
