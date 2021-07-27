ea4bf81a50552dcb5c5fd3da98d67530160df60a
Den375 <denisoff375@gmail.com>
2021-07-27 23:37:02 +0300

    Updates

---
+++ precache-manifest.ff79c97a6016666efa5a8dba321e5c86.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "06236a07156cc1ca8b79",
+    "url": "/react-app/static/css/main.39fcbf0e.chunk.css"
+  },
+  {
+    "revision": "06236a07156cc1ca8b79",
+    "url": "/react-app/static/js/main.06236a07.chunk.js"
+  },
+  {
+    "revision": "0b05eb1517f225ebcf79",
+    "url": "/react-app/static/css/1.b199694e.chunk.css"
+  },
+  {
+    "revision": "0b05eb1517f225ebcf79",
+    "url": "/react-app/static/js/1.0b05eb15.chunk.js"
+  },
+  {
+    "revision": "5b9daacb9fafa62f824e",
+    "url": "/react-app/static/css/2.972a520f.chunk.css"
+  },
+  {
+    "revision": "5b9daacb9fafa62f824e",
+    "url": "/react-app/static/js/2.5b9daacb.chunk.js"
+  },
+  {
+    "revision": "77561ca694de1e260d5c",
+    "url": "/react-app/static/css/3.f5c98c27.chunk.css"
+  },
+  {
+    "revision": "77561ca694de1e260d5c",
+    "url": "/react-app/static/js/3.77561ca6.chunk.js"
+  },
+  {
+    "revision": "38bedff378c4132c5bed",
+    "url": "/react-app/static/js/4.38bedff3.chunk.js"
+  },
+  {
+    "revision": "c60eb1a4e62589efdccf",
+    "url": "/react-app/static/js/runtime~main.c60eb1a4.js"
+  },
+  {
+    "revision": "1088ab62b3271e6f12b72ee60c56e3f7",
+    "url": "/react-app/static/media/126.1088ab62.svg"
+  },
+  {
+    "revision": "1aa154476b6e7ab8a8609712570534e5",
+    "url": "/react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.52e7661d752d50a0b56427c497e5856e.js"
+  "/react-app/precache-manifest.ff79c97a6016666efa5a8dba321e5c86.js"
 );
 
 workbox.clientsClaim();
